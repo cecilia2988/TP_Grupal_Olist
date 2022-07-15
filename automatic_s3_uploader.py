7839a12232b12c7845ccd0ba13380c0edc605f5b
cecilia2988 <cecilia2988@gmail.com>
2022-07-15 13:51:57 -0300

    merge con rama desarrollo

--- automatic_s3_uploader.py
+++ automatic_s3_uploader.py
@@ -8,5 +8,5 @@ client = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_ke
 for file in os.listdir():
 	if '.csv' in file:
 		upload_file_bucket = 'tpbucketg8'
-		upload_file_key= 'data/' + str(file)
+		upload_file_key= 'data/' + str(file) + '/' + str(file)
 		client.upload_file(file,upload_file_bucket,upload_file_key)
